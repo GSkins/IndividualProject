22d6280b70f0760912ee0c0859e3a514ec6454ae
GSkins <55387294+GSkins@users.noreply.github.com>
2019-10-21 15:14:04 +0100

    Add files via upload

--- startup.sh
+++ startup.sh
@@ -19,6 +19,10 @@ pip3 install flask-login
 pip3 install flask-bcrypt
 pip3 install flask_wtf
 pip3 install DateTime
+pip3 install PILLOW
+pip install python-secrets
+pip3 install python-secrets
+pip install python2-secrets
 pip3 install pyopenssl
 #openssl req -x509 -newkey rsa:4096 -nodes -out cert.pem -keyout key.pem -days 365
 #export FLASK_ENV=production FLASK_RUN_HOST=0.0.0.0 FLASK_RUN_PORT=5000 FLASK_RUN_CERT=cert.pem FLASK_RUN_KEY=key.pem
