8ed42109d4992672fff96f1cc0943a517689306b
GSkins <55387294+GSkins@users.noreply.github.com>
2019-10-31 16:42:39 +0000

    Delete install-docker.sh

--- flaskblog/scripts/install-docker.sh
+++
@@ -1,8 +0,0 @@
-
-# Linux Installation
-
-curl https://get.docker.com | sudo bash
-# Add yourself to the docker group, the remote script suggested above will
-# Have already created the docker group for you
-sudo usermod -aG docker $(whoami)
-# Restart the shell
